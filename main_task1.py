83d4308a3c524a247dd9b3f0bc1744bd43c5f292
Nikhil Kalidasu <skdc13129@gmail.com>
2024-07-08 17:29:10 -0500

    task 1 implemented

--- main_task1.py
+++ main_task1.py
@@ -1,23 +1,31 @@
-from __future__ import print_function
-
 import sys
-from operator import add
-
 from pyspark import SparkContext
+
+
+def seqOp(acc: set, value: str) -> set:
+    acc.add(value)
+    return acc
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: wordcount <file> <output> ", file=sys.stderr)
-        exit(-1)
-        
+        sys.exit(-1)
+
     sc = SparkContext(appName="PythonWordCount")
     lines = sc.textFile(sys.argv[1], 1)
-    
-    counts = lines.flatMap(lambda x: x.split(' ')).map(lambda x: (x, 1)).reduceByKey(add)
-    
-    counts.saveAsTextFile(sys.argv[2])
-    
-    output = counts.collect()
-    
+    n = 10
+
+    counts = lines \
+        .map(lambda line: tuple(line.split(',')[:2])) \
+        .aggregateByKey(set(), seqOp, lambda a, b: a.union(b)) \
+        .mapValues(len)
+    top_n = counts.takeOrdered(n, key=lambda x: -x[1])  # Negate x[1] to get in descending order
+
+    sc.parallelize(top_n).saveAsTextFile(sys.argv[2])
     sc.stop()
+
+    # Print top_n
+    print(f"Top {n} medallions with the most drivers:")
+    for medallion, count in top_n:
+        print(f"Medallion: {medallion}, Unique Drivers: {count}")
