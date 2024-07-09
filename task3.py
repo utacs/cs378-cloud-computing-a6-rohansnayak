d3d7eea2b980f6d805990821e0bc4bfc8d855689
Nayak, Rohan <rohannayak@Rohans-MacBook-Pro-2.local>
2024-07-08 19:20:04 -0500

    Added Task 3 Possible Solution

--- task3.py
+++ task3.py
@@ -52,3 +52,58 @@ if __name__ == "__main__":
     print(f"Top {n} medallions with highest earnings per minute:")
     for hour, profit_ratio in top_n_hours:
         print(f"Hour: {hour}, Profit Ratio: {profit_ratio}")
+
+
+# import sys
+# from pyspark import SparkContext
+# from datetime import datetime
+
+# def mapper(line: str) -> tuple[str, tuple[float, float]]:
+#     """
+#     Returns hour, (surcharge, distance)
+#     """
+#     parts = line.split(',')
+#     if len(parts) < 18:
+#         return None
+#     try:
+#         # Extract datetime and surcharge amount and distance
+#         datetime_str = parts[3]
+#         surcharge = float(parts[15])
+#         distance = float(parts[9])
+#         hour = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S').hour
+#         return hour, (surcharge, distance)
+#     except BaseException:  # pylint: disable=broad-exception-caught
+#         return None
+
+# def clean_data(x: tuple):
+#     if not x or len(x) != 2:
+#         return False
+#     _, (surcharge, distance) = x
+#     return distance > 0 and surcharge >= 0
+
+# def reducer(cum: tuple[float, float], value: tuple[float, float]) -> tuple[float, float]:
+#     return (cum[0] + value[0], cum[1] + value[1])
+
+# if __name__ == "__main__":
+#     if len(sys.argv) != 3:
+#         print("Usage: <file> <output> ", file=sys.stderr)
+#         sys.exit(-1)
+
+#     sc = SparkContext(appName="ProfitRatioPerHour")
+#     lines = sc.textFile(sys.argv[1], 1)
+#     n = 3
+
+#     profit_ratio = lines \
+#         .map(mapper) \
+#         .filter(clean_data) \
+#         .reduceByKey(reducer) \
+#         .mapValues(lambda i: i[0] / i[1])  # Profit ratio = Surcharge / Distance
+#     top_n = profit_ratio.takeOrdered(n, key=lambda x: -x[1])  # Negate x[1] to get in descending order
+
+#     sc.parallelize(top_n).saveAsTextFile(sys.argv[2])
+#     sc.stop()
+
+#     # Print top_n
+#     print(f"Top {n} hours with highest profit ratio:")
+#     for hour, ratio in top_n:
+#         print(f"Hour: {hour}, Profit Ratio: {ratio}")
